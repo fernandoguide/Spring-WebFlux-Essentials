e412a108d8b2700f2a5662eaf4884b237e065c10
fernandoguide <fernando-guide@hotmail.com>
2020-08-17 19:17:09 -0300

    first commit

---
+++ src/main/java/br/com/fernando/webflux/SpringWebFluxEssentialsApplication.java
@@ -0,0 +1,13 @@
+package br.com.fernando.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebFluxEssentialsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebFluxEssentialsApplication.class, args);
+    }
+
+}
